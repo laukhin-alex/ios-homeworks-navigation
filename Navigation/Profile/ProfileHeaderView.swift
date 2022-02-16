dcaaf68363418ecbe8b3ea813b57584801fee66b
laukhin-alex <alex.laukhin@yandex.ru>
2022-02-16 19:56:02 +0300

    TextField was added

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ProfileHeaderView: UIView {
+class ProfileHeaderView: UIView, UITextFieldDelegate {
 
     @IBOutlet weak var avatarImage: UIImageView!
 
@@ -15,19 +15,27 @@ class ProfileHeaderView: UIView {
     
     @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
 
+    @IBOutlet weak var statusTextChanging: UITextField!
+
     @IBOutlet weak var statusText: UILabel!
 
     private var isExpanded = true
 
-    let statusTextChanging = UITextField()
+    private var statusTextPrivate: String = " "
+
+
+    //    let statusTextChanging = UITextField(frame: CGRect(x: 150, y: 145, width: 200, height: 40))
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.setupView()
+        self.statusTextChanging.isHidden = true
+
     }
 
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+
 
     }
 
@@ -48,6 +56,7 @@ class ProfileHeaderView: UIView {
         showStatusButton.layer.shadowRadius = 4
         showStatusButton.layer.shadowColor = UIColor.black.cgColor
         showStatusButton.layer.shadowOpacity = 0.7
+        showStatusButton.tag = 0
 
         statusText.text = "Good news, everyone!"
 
@@ -61,21 +70,44 @@ class ProfileHeaderView: UIView {
 
     @IBAction func buttonIsTyped(_ sender: UIButton) {
         print(statusText.text ?? "OK")
-
         self.bottomConstraint.constant = self.isExpanded ? 100 : 34
-        UIView.animate(withDuration: 1) {
+        UIView.animate(withDuration: 0.1) {
             self.layoutIfNeeded()
         } completion: { _ in
             self.isExpanded.toggle()
             if self.bottomConstraint.constant == 100 {
                 self.showStatusButton.setTitle("Set status", for: .normal)
-                self.addSubview(self.statusTextChanging)
+                self.statusTextChangingSetup()
+                self.statusTextChanging.isHidden = false
             } else {
-                self.showStatusButton.setTitle("Set status", for: .normal)
+                self.showStatusButton.setTitle("Show status", for: .normal)
+
+                self.statusText.text = self.statusTextChanging.text ?? "Wrong type"
+                if self.statusText.text == "" {
+                    self .statusText.text = " "
+                }
+                self.statusTextChanging.isHidden = true
             }
         }
+    }
+    private func statusTextChangingSetup() {
 
 
+        self.statusTextChanging.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        self.statusTextChanging.layer.cornerRadius = 12
+        self.statusTextChanging.backgroundColor = .white
+        self.statusTextChanging.placeholder = "type your status here"
+        self.statusTextChanging.font = UIFont.systemFont(ofSize: 15)
+        self.statusTextChanging.autocorrectionType = UITextAutocorrectionType.no
+        self.statusTextChanging.keyboardType = .default
+        self.statusTextChanging.returnKeyType = UIReturnKeyType.done
+        self.statusTextChanging.clearButtonMode = UITextField.ViewMode.whileEditing
+        self.statusTextChanging.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
+        self.statusTextChanging.keyboardAppearance = .default
+        self.statusTextChanging.layer.borderColor = UIColor.black.cgColor
+        self.statusTextChanging.borderStyle = .none
+        self.statusTextChanging.layer.borderWidth = 1
+        self.statusTextChanging.delegate = self
 
     }
 }
