8434e29f83fdbb759010a890658086cd3b9de33c
laukhin-alex <alex.laukhin@yandex.ru>
2022-02-19 17:15:20 +0300

    HW4 is finished

--- Navigation/FeedViewController.swift
+++ Navigation/FeedViewController.swift
@@ -8,31 +8,79 @@
 import UIKit
 
 class FeedViewController: UIViewController {
-
+    
+    private lazy var verticalStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var postButtonOne: UIButton = {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(self.goToPost), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.clipsToBounds = true
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 12
+        button.setTitle("Показать пост, кнопка один", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var postButtonTwo: UIButton = {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(self.goToPost), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.clipsToBounds = true
+        button.backgroundColor = .systemRed
+        button.layer.cornerRadius = 12
+        button.setTitle("Показать пост, кнопка два", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        self.view.backgroundColor = .systemYellow
-        let postButton = UIButton(type: .system) as UIButton
-        postButton.clipsToBounds = true
-        postButton.backgroundColor = .blue
-        postButton.layer.cornerRadius = 12
-        postButton.setTitle("Показать пост", for: .normal)
-        postButton.setTitleColor(.systemYellow, for: .normal)
-        postButton.translatesAutoresizingMaskIntoConstraints = false
-        self.view.addSubview(postButton)
-        postButton.bottomAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 0).isActive = true
-        postButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20).isActive = true
-        postButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20).isActive = true
-        postButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        postButton.addTarget(self, action: #selector(makeAction), for: .touchUpInside)
-
+        
+        self.view.backgroundColor = .systemBackground
+        self.drawSelf()
+        
     }
-    @objc func makeAction(sender:UIButton!)  {
+    
+    private func drawSelf() {
+        self.view.addSubview(self.verticalStackView)
+        self.verticalStackView.addArrangedSubview(postButtonOne)
+        self.verticalStackView.addArrangedSubview(postButtonTwo)
+        
+        let topConstraint = self.verticalStackView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        let leadingConstraint = self.verticalStackView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20)
+        let trailingConstraint = self.verticalStackView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20)
+        let heightConstraint = self.verticalStackView.heightAnchor.constraint(equalToConstant: 200)
+        
+        let leadingButtonOneConstraint = self.postButtonOne.leadingAnchor.constraint(equalTo: self.verticalStackView.leadingAnchor)
+        let trailingButtonOneConstraint = self.postButtonOne.trailingAnchor.constraint(equalTo: self.verticalStackView.trailingAnchor)
+        let bottomButtonOneConstraint = self.postButtonOne.topAnchor.constraint(equalTo: self.verticalStackView.topAnchor)
+        
+        
+        let leadingButtonTwoConstraint = self.postButtonTwo.leadingAnchor.constraint(equalTo: self.verticalStackView.leadingAnchor)
+        let trailingButtonTwoConstraint = self.postButtonTwo.trailingAnchor.constraint(equalTo: self.verticalStackView.trailingAnchor)
+        
+        
+        NSLayoutConstraint.activate([
+            topConstraint, leadingConstraint, trailingConstraint, leadingButtonOneConstraint, trailingButtonOneConstraint, bottomButtonOneConstraint, leadingButtonTwoConstraint, trailingButtonTwoConstraint, heightConstraint
+        ].compactMap({ $0 }))
+    }
+    
+    @objc func goToPost(sender:UIButton!)  {
         let postViewController  = PostViewController()
-
+        
         self.navigationController?.pushViewController(postViewController, animated: true)
-
+        
         self.navigationItem.backButtonTitle = "Назад"
     }
 }
--- Navigation/ProfileHeaderView.swift
+++ Navigation/ProfileHeaderView.swift
@@ -5,4 +5,117 @@
 //  Created by Александр on 19.02.2022.
 //
 
-import Foundation
+import UIKit
+
+protocol ProfileHeaderViewProtocol: AnyObject {
+    func didTapStatusButton(textFieldIsVisible: Bool, completion: @escaping () -> Void)
+}
+
+class ProfileHeaderView: UIView {
+
+    private lazy var avatarImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .systemRed
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
+    private lazy var fullNameLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .systemGreen
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private lazy var statusLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .blue
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private lazy var statusTextField: UITextField = {
+        let textField = UITextField()
+        textField.backgroundColor = .systemYellow
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+
+    private lazy var setStatusButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemOrange
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    private lazy var labelsStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+
+    private lazy var infoStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 20
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+
+    private var buttonTopConstraint: NSLayoutConstraint?
+
+    weak var delegate: ProfileHeaderViewProtocol?
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.drawSelf()
+    }
+    override func layoutSubviews() {}
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func drawSelf() {
+        self.addSubview(self.infoStackView)
+        self.addSubview(self.setStatusButton)
+        self.addSubview(self.statusTextField)
+        self.infoStackView.addArrangedSubview(self.avatarImageView)
+        self.infoStackView.addArrangedSubview(self.labelsStackView)
+        self.labelsStackView.addArrangedSubview(self.fullNameLabel)
+        self.labelsStackView.addArrangedSubview(self.statusLabel)
+        self.labelsStackView.addArrangedSubview(self.statusTextField)
+        self.backgroundColor = .brown
+
+        let topConstraint = self.infoStackView.topAnchor.constraint(equalTo: self.topAnchor, constant: 10)
+        let leadingConstraint = self.infoStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20)
+        let trailingConstraint = self.infoStackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20)
+
+        let imageViewAspectRatio = self.avatarImageView.heightAnchor.constraint(equalTo: self.avatarImageView.widthAnchor, multiplier: 1.0)
+
+        self.buttonTopConstraint = self.setStatusButton.topAnchor.constraint(equalTo: self.infoStackView.bottomAnchor, constant: 20)
+        self.buttonTopConstraint?.priority = UILayoutPriority(rawValue: 999)
+        let leadingButtonConstraint = self.setStatusButton.leadingAnchor.constraint(equalTo: self.infoStackView.leadingAnchor)
+        let trailingButtonConstraint = self.setStatusButton.trailingAnchor.constraint(equalTo: self.infoStackView.trailingAnchor)
+        let bottomButtonConstraint = self.setStatusButton.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10)
+        let heightButtonConstraint = self.setStatusButton.heightAnchor.constraint(equalToConstant: 40)
+        let topStatusTextConstraint = self.statusTextField.topAnchor.constraint(equalTo: self.infoStackView.bottomAnchor, constant: 10)
+
+        let leadingStatusTextConstraint = self.statusTextField.leadingAnchor.constraint(equalTo: self.statusLabel.leadingAnchor)
+        let trailingStatusTextConstraint = self.statusTextField.trailingAnchor.constraint(equalTo: self.infoStackView.trailingAnchor)
+        let heightTextStatusTextFieldConstraint = self.statusTextField.heightAnchor.constraint(equalToConstant: 34)
+
+        
+
+
+        NSLayoutConstraint.activate([
+            topConstraint, leadingConstraint, trailingConstraint,
+            imageViewAspectRatio,
+            self.buttonTopConstraint, leadingButtonConstraint,
+            trailingButtonConstraint, bottomButtonConstraint, heightButtonConstraint, topStatusTextConstraint, leadingStatusTextConstraint,trailingStatusTextConstraint, heightTextStatusTextFieldConstraint
+        ].compactMap({ $0 }))
+    }
+}
